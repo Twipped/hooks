6bfb4d406d48f3f051294b5ff2f8b6c566d090f6
Jocelyn Badgley (Twipped) <joc@twipped.com>
2022-02-20 19:18:57 -0800

    Rewrote useToggledState to use a memoized api instead of callback hooks

--- hooks/useToggledState.js
+++ hooks/useToggledState.js
@@ -1,29 +1,72 @@
 
-import { useCallback } from 'react';
 import useGettableState from './useGettableState';
+import useStableMemo from './useStableMemo';
+
+
+/**
+ * @typedef ToggledState
+ * @description Toggled state API interface
+ */
 
 /**
  * Produces a state hook that can only hold a boolean value.
- * Invoking the setter without a value toggles it.
- * @param  {Boolean} initial
+ * Provides callbacks for toggling, activating and deactivating the state.
+ *
+ * @param  {boolean} initial
+ * @returns {ToggledState}
  */
 export default function useToggledState (initial = false) {
   const [ state, setter, getter ] = useGettableState(!!initial);
 
-  const toggle = useCallback((onoff) => {
-    if (onoff !== true && onoff !== false) onoff = !getter();
-    setter(onoff);
-    return onoff;
-  });
+  return useStableMemo(() => ({
 
-  const toggleOff = useCallback(() => { setter(false); });
-  const toggleOn  = useCallback(() => { setter(true); });
+    /**
+     * @property {boolean} ToggledState#state
+     * @returns {boolean} The current state
+     */
+    get state () {
+      return state;
+    },
 
-  return {
-    state,
-    toggle,
-    on: toggleOn,
-    off: toggleOff,
+    /**
+     * Toggles the state on or off based on current value
+     *
+     * @function ToggledState#toggle
+     * @param  {boolean} [onoff] Force a specific value. Will be ignored if value is not a boolean.
+     * @returns {boolean}
+     */
+    toggle (onoff) {
+      if (onoff !== true && onoff !== false) onoff = !getter();
+      setter(onoff);
+      return onoff;
+    },
+
+    /**
+     * Sets state to true
+     *
+     * @function ToggledState#on
+     * @returns {void}
+     */
+    on () {
+      setter(true);
+    },
+
+    /**
+     * Sets state to false
+     *
+     * @function ToggledState#off
+     * @returns {void}
+     */
+    off () {
+      setter(false);
+    },
+
+    /**
+     * Retreives the current state
+     *
+     * @function ToggledState#toggle
+     * @returns {boolean}
+     */
     get: getter,
-  };
+  }));
 }
