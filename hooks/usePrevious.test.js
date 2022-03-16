4b551dc5901a762931b45916596d303b0740c792
Jocelyn Badgley <joc@twipped.com>
2022-03-16 12:18:59 -0700

    chore: Added the start of a test suite

---
+++ hooks/usePrevious.test.js
@@ -0,0 +1,27 @@
+import { test } from 'tap';
+import { renderHook } from '@testing-library/react-hooks';
+import usePrevious from './usePrevious.js';
+
+test('should always return previous state after each update', async (t) => {
+  const { result, rerender } = renderHook((props) => usePrevious(props.state), { initialProps: { state: 0 } });
+
+  t.equal(result.current, undefined, 'first execute should be undefined');
+
+  rerender({ state: 2 });
+  t.equal(result.current, 0, 'second render shows first value');
+
+  rerender({ state: 4 });
+  t.equal(result.current, 2, 'third render shows second value');
+});
+
+test('accepts an initial value', async (t) => {
+  const { result, rerender } = renderHook((props) => usePrevious(props.state, 10), { initialProps: { state: 0 } });
+
+  t.equal(result.current, 10, 'first execute should be undefined');
+
+  rerender({ state: 2 });
+  t.equal(result.current, 0, 'second render shows first value');
+
+  rerender({ state: 4 });
+  t.equal(result.current, 2, 'third render shows second value');
+});
