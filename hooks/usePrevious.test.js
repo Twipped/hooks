d7e0669fbaec4e466522b0c3272308a8100bf9f7
Jocelyn Badgley <jocelyn.badgley@zenbusiness.com>
2022-06-27 14:19:28 -0700

    chore: Fix failing tests

--- hooks/usePrevious.test.js
+++ hooks/usePrevious.test.js
@@ -1,9 +1,12 @@
+import 'global-jsdom/register';
 import { test } from 'tap';
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react';
 import usePrevious from './usePrevious.js';
 
 test('should always return previous state after each update', async (t) => {
-  const { result, rerender } = renderHook((props) => usePrevious(props.state), { initialProps: { state: 0 } });
+  const { result, rerender, unmount } = renderHook((props) => usePrevious(props.state), {
+    initialProps: { state: 0 },
+  });
 
   t.equal(result.current, undefined, 'first execute should be undefined');
 
@@ -12,10 +15,14 @@ test('should always return previous state after each update', async (t) => {
 
   rerender({ state: 4 });
   t.equal(result.current, 2, 'third render shows second value');
+
+  unmount();
 });
 
 test('accepts an initial value', async (t) => {
-  const { result, rerender } = renderHook((props) => usePrevious(props.state, 10), { initialProps: { state: 0 } });
+  const { result, rerender } = renderHook((props) => usePrevious(props.state, 10), {
+    initialProps: { state: 0 },
+  });
 
   t.equal(result.current, 10, 'first execute should be undefined');
 
