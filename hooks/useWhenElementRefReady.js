0b5202b335cfed9facd07b22526da0510a02aed1
Jocelyn Badgley (Twipped) <joc@twipped.com>
2023-03-11 20:01:41 -0800

    fix(useWhenElementRefReady): Now actually works!

--- hooks/useWhenElementRefReady.js
+++ hooks/useWhenElementRefReady.js
@@ -2,12 +2,13 @@ import { useState, useRef } from 'react';
 import useIsomorphicEffect from './useIsomorphicEffect.js';
 import useEventCallback from './useEventCallback.js';
 import resolveRef from './resolveRef.js';
+/** @typedef {import('@types/react').Ref} Ref */
 
 /**
  * Executes the passed function once the provided Ref resolves to a value.
  *
  * @function useWhenElementRefReady
- * @param {Ref} ref
+ * @param {Ref|HTMLElement} ref Ref
  * @param {Function} [onResolved] Callback to invoke with the ref resolves
  * @returns {any}
  */
@@ -17,13 +18,14 @@ export default function useWhenElementRefReady (ref, onResolved) {
   onResolved = useEventCallback(onResolved);
 
   useIsomorphicEffect(() => {
+    if (!ref) return;
     const nextRef = resolveRef(ref);
     if (nextRef === resolvedRef) return;
 
     if (typeof cleanupRef.current === 'function') cleanupRef.current();
     if (onResolved) cleanupRef.current = onResolved(nextRef);
     setRef(nextRef);
-  }, [ ref, ref.current, resolvedRef ]);
+  });
 
   return resolvedRef;
 }
