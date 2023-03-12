1aa4a9db9d0e32c15e655ce4b13c2e22930a25d4
Jocelyn Badgley (Twipped) <joc@twipped.com>
2023-03-11 19:59:00 -0800

    feat(useLocalStorage): Now accepts a third argument for options.
    Passing `isJSON: false` treats the state as a plain string without JSON serialization.

--- hooks/useLocalStorage.js
+++ hooks/useLocalStorage.js
@@ -6,11 +6,15 @@ import { useWebStorageApi, storageKeys } from './webStorageApi.js';
  *
  * @function useLocalStorage
  * @param  {string} key          Name of the key to store the value into
- * @param  {any} defaultValue    The initial value to use if the key does not exist.
- * @returns {StateHookInterface} A three item array containing: state, setState, getState
+ * @param  {any} [defaultValue]    The initial value to use if the key does not exist.
+ * @param  {Object} [options]
+ * @param  {boolean} [options.isJSON] Controls if the value should be
+ * serialized/deserialized as JSON. Defaults to true.
+ * @returns {[state: any, setState: Function, getState: Function]} A three item
+ * array containing: state, setState, getState
  */
-export default function useLocalStorage (key, defaultValue) {
-  return useWebStorageApi(window.localStorage, key, defaultValue);
+export default function useLocalStorage (key, defaultValue, options) {
+  return useWebStorageApi(window.localStorage, key, defaultValue, options);
 }
 
 /**
--- hooks/webStorageApi.js
+++ hooks/webStorageApi.js
@@ -1,6 +1,7 @@
 /* global StateHookInterface */
 import { useCallback } from 'react';
 import jsonSoftParse from '@twipped/utils/jsonSoftParse';
+import DEFAULT from '@twipped/utils/default';
 import { useWindowEventListener } from './useGlobalListener.js';
 import useDerivedState from './useDerivedState.js';
 
@@ -15,29 +16,56 @@ import useDerivedState from './useDerivedState.js';
  * @param {Storage} store Which API to use
  * @param {string} key Name of the key to store the value into
  * @param {any} [defaultValue] The initial value to use if the key does not exist
+ * @param {Object} options
+ * @param {boolean} options.isJSON
  * @returns {StateHookInterface} A three item array containing: state, setState, getState
  * @private
  */
-export function useWebStorageApi (store, key, defaultValue) {
+export function useWebStorageApi (store, key, defaultValue = DEFAULT, { isJSON = true } = {}) {
+  if (defaultValue !== DEFAULT && defaultValue !== null && !isJSON && typeof defaultValue !== 'string') {
+    throw new TypeError('defaultValue cannot be anything other than string when using isJSON=false');
+  }
   const [ value, writeValue, getValue ] = useDerivedState(() => {
-    const stored = jsonSoftParse(store.getItem(key));
-    return stored || stored === 0 ? stored : defaultValue;
+    let stored = isJSON
+      ? jsonSoftParse(store.getItem(key))
+      : store.getItem(key);
+
+    if (
+      defaultValue !== DEFAULT
+      && defaultValue !== null
+      && (stored === null || stored === undefined)
+    ) {
+      stored = defaultValue;
+      if (isJSON) {
+        store.setItem(key, JSON.stringify(defaultValue));
+      } else {
+        store.setItem(key, defaultValue);
+      }
+    }
+
+    return stored;
   }, [ store, key ]);
 
   const setValue = useCallback((newValue) => {
+    if (newValue && !isJSON && typeof newValue !== 'string') {
+      throw new TypeError('setValue cannot receive anything other than string or undefined when using isJSON=false');
+    }
+
     writeValue(() => {
-      if (newValue === undefined) {
+      if (newValue === undefined || newValue === null) {
         store.removeItem(key);
-      } else {
+      } else if (isJSON) {
         store.setItem(key, JSON.stringify(newValue));
+      } else {
+        store.setItem(key, newValue);
       }
       return newValue;
     });
-  }, [ key, store, writeValue ]);
+  }, [ key, store, writeValue, isJSON ]);
 
   useWindowEventListener('storage', (ev) => {
     if (ev.storageArea === store && ev.key === key) {
-      writeValue(ev.newValue === null ? defaultValue : JSON.parse(ev.newValue));
+      setValue(ev.newValue === null ? defaultValue : JSON.parse(ev.newValue));
     }
   });
 
